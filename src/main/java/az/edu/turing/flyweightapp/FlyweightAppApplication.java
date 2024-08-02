3c5f5abe685497a051a7aa98552307d1ec08dd63
HP ProBook <kalbaliyevaselen@gmai.com>
2024-08-02 17:08:43 +0400

    first commit

---
+++ src/main/java/az/edu/turing/flyweightapp/FlyweightAppApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.flyweightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyweightAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyweightAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/edu/turing/flyweightapp/FlyweightAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.flyweightapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyweightAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
